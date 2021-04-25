82831ed243832729c1e374ee37ab206eb0522dbc
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-24 21:09:51 -0400

    chalk

---
+++ app.js
@@ -0,0 +1,11 @@
+// const validator = require("validator")
+const fs = require("fs")
+const clk = require("chalk")
+const getNotes =require("./notes")
+
+// console.log(getNotes())
+// fs.appendFileSync("notes.txt", " from me")
+// fs.writeFileSync("notes.text", "hello world")
+// console.log(validator.isEmail("abcmail.co"));
+
+console.log(clk.bold.blue("hello world"));
