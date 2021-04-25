c5b7ed8f7b62d9b90216b0ac34bc0bdd5068fde6
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-25 10:37:56 -0400

    process.argv

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // const validator = require("validator")
 const fs = require("fs")
 const clk = require("chalk")
-const getNotes =require("./notes")
-
+const getNotes = require("./notes")
+const name = "chenyun"
 // console.log(getNotes())
 // fs.appendFileSync("notes.txt", " from me")
 // fs.writeFileSync("notes.text", "hello world")
 // console.log(validator.isEmail("abcmail.co"));
+// console.log(clk.bold.blue("hello world"));
+// console.log(process.argv)
 
-console.log(clk.bold.blue("hello world"));
+const command = process.argv[2];
+if (command === "add") {
+    console.log("Adding note")
+} else if (command === "remove") {
+    console.log("removing note")
+}
